a594931e2873902433dd76685b0ba308b86c215a
Abhijit Dhara <51172366+cybertron21@users.noreply.github.com>
2019-12-31 02:41:19 +0530

    Add files via upload

---
+++ Python_GUI/window.py
@@ -0,0 +1,5 @@
+from tkinter import *
+root = Tk()      #to create a window
+theLabel = Label(root, text = "This is the beginning")  #for the content to be presented on window
+theLabel.pack()     #combining together
+root.mainloop()     #continuouly running through loops
